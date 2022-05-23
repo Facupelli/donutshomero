f667d8c0db34265d50cc6dde9f12fa75b5b7af27
Facupelli <facundopellicer4@gmail.com>
2022-05-23 15:14:57 -0300

    contacto page

--- donutshomero/pages/contacto/index.jsx
+++ donutshomero/pages/contacto/index.jsx
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import Footer from "../../components/Footer/Footer";
 import Nav from "../../components/Nav/Nav";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 import s from "./index.module.scss";
 
 export default function Contact() {
@@ -11,8 +16,32 @@ export default function Contact() {
       </Head>
       <Nav route="contacto" />
       <div className={s.info}>
-        <p>DEJANOS TU MENSAJE</p>
-        <p>SEGUINOS EN LAS REDES</p>
+        <div className={s.message}>
+          <p className={s.title}>DEJANOS TU MENSAJE</p>
+          <span>+54 264-4415016</span>
+          <div>
+            <a>
+              <FontAwesomeIcon icon={faPhone} width="40px" />
+            </a>
+            <a
+              href="https://wa.me/542644415016"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faWhatsapp} width="50px" />
+            </a>
+          </div>
+        </div>
+        <div className={s.networks}>
+          <p className={s.title}>SEGUINOS EN LAS REDES</p>
+          <a
+            href="https://www.instagram.com/donuts_homero/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} width="50px" />
+          </a>
+        </div>
       </div>
       <Footer route="contacto" />
     </div>
