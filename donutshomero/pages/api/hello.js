89a34ffd6c4ded0c0537800d249a294830d53d0e
Facupelli <facundopellicer4@gmail.com>
2022-06-02 19:12:06 -0300

    sql models

--- donutshomero/pages/api/hello.js
+++ donutshomero/pages/api/hello.js
@@ -1,6 +1,8 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-  const feed = await prisma.donutsBuyedByUsers.findMany({})
-  res.status(200).json({ name: feed })
+  const buy1 = await prisma.donutsPromoByUsers.findMany({})
+  const buy2 = await prisma.donutsByUsers.findMany({})
+
+  res.status(200).json({ donuts: buy2, donutsPromo: buy1 })
 }
