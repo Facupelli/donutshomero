2d3a912536525e002596bdd712996c2525cb5c2e
Facupelli <facundopellicer4@gmail.com>
2022-06-04 13:57:30 -0300

    checkout api

--- donutshomero/pages/api/checkout/index.js
+++ donutshomero/pages/api/checkout/index.js
@@ -7,83 +7,91 @@ mercadopago.configure({
 
 export default async function handler(req, res) {
   // Crea un objeto de preferencia
-  const { cart, customerData } = req.body;
+  if (req.method === "POST") {
+    try {
+      const { cart, customerData } = req.body;
 
-  const items = cart.map((item) => {
-    return {
-      id: item.id,
-      title: item.name ? item.name : item.title,
-      quantity: Number(item.quantity),
-      unit_price: Number(item.price),
-    };
-  });
+      const items = cart.map((item) => {
+        return {
+          id: item.id,
+          title: item.name ? item.name : item.title,
+          quantity: Number(item.quantity),
+          unit_price: Number(item.price),
+        };
+      });
 
-  // console.log('ITEMS', items)
+      // console.log('ITEMS', items)
 
-  let preference = {
-    items,
-    payer: {
-      name: customerData.fullName.split(" ")[0],
-      surname: customerData.fullName.split(" ")[1],
-      email: customerData.email,
-      phone: {
-        area_code: "264",
-        number: customerData.phone,
-      },
-      identification: {
-        type: "DNI",
-        number: "",
-      },
-      address: {
-        street_name: customerData.address,
-        street_number: Number(customerData.number),
-        zip_code: "5400",
-      },
-    },
-    back_urls: {
-      success:
-        process.env.NODE_ENV === "production"
-          ? "https://donutshomero.vercel.app"
-          : "http://localhost:3000/",
-      // failure: "http://www.failure.com",
-      // pending: "http://www.pending.com",
-    },
-    auto_return: "approved",
-    payment_methods: {
-      excluded_payment_types: [
-        {
-          id: "credit_card",
+      let preference = {
+        items,
+        payer: {
+          name: customerData.fullName.split(" ")[0],
+          surname: customerData.fullName.split(" ")[1],
+          email: customerData.email,
+          phone: {
+            area_code: "264",
+            number: customerData.phone,
+          },
+          identification: {
+            type: "DNI",
+            number: "",
+          },
+          address: {
+            street_name: customerData.address,
+            street_number: Number(customerData.number),
+            zip_code: "5400",
+          },
         },
-        {
-          id: "debit_card",
+        back_urls: {
+          success:
+            process.env.NODE_ENV === "production"
+              ? "https://donutshomero.vercel.app"
+              : "http://localhost:3000/",
+          // failure: "http://www.failure.com",
+          // pending: "http://www.pending.com",
         },
-        {
-          id: "ticket",
+        auto_return: "approved",
+        payment_methods: {
+          excluded_payment_types: [
+            {
+              id: "credit_card",
+            },
+            {
+              id: "debit_card",
+            },
+            {
+              id: "ticket",
+            },
+          ],
+
+          installments: 12,
         },
-      ],
+      };
 
-      installments: 12,
-    },
-  };
+      const response = await mercadopago.preferences.create(preference);
 
-  const response = await mercadopago.preferences.create(preference);
+      res.json({
+        id: response.body.id,
+        init_point: response.body.init_point,
+      });
 
-  res.json({
-    id: response.body.id,
-    init_point: response.body.init_point,
-  });
+      // res.redirect(302, response.body.init_point);
 
-  // res.redirect(302, response.body.init_point);
-
-  // mercadopago.preferences
-  //   .create(preference)
-  //   .then((res) => {
-  //     res.json({
-  //       id: response.body.id,
-  //       init_point: response.body.init_point,
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     console.log("MERCADOPAGO:", error);
-  //   });
+      // mercadopago.preferences
+      //   .create(preference)
+      //   .then((res) => {
+      //     res.json({
+      //       id: response.body.id,
+      //       init_point: response.body.init_point,
+      //     });
+      //   })
+      //   .catch((error) => {
+      //     console.log("MERCADOPAGO:", error);
+      //   });
+    } catch (err) {
+      res.status(400).json({ error: err });
+    }
+  } else {
+    res.status(500).json({ error: "only post" });
+  }
 }
