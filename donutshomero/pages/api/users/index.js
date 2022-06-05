892fe7b7217e56dc7b2b3ce012cf7d36e3b1f899
Facupelli <facundopellicer4@gmail.com>
2022-06-05 15:44:27 -0300

    api get user found

---
+++ donutshomero/pages/api/users/index.js
@@ -0,0 +1,35 @@
+import prisma from "../../../lib/prisma";
+import axios from "axios";
+
+export default async function usersController(req, res) {
+  // Crea un objeto de preferencia
+  if (req.method === "GET") {
+    try {
+      const phoneNumber = req.query.number;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          phone: phoneNumber,
+        },
+        select: {
+          name: true,
+          surname: true,
+          address: true,
+          addressNumber: true,
+          phone: true,
+          ubiLink: true,
+        },
+      });
+
+      if (!user) {
+        res.json({ message: "user not found" });
+      } else {
+        res.json(user);
+      }
+    } catch (err) {
+      res.status(400).json({ error: err });
+    }
+  } else {
+    res.status(500).json({ error: "only get" });
+  }
+}
