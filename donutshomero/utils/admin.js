4fa446dc852139a9ba775012e03bc0075b87c990
Facupelli <facundopellicer4@gmail.com>
2022-06-10 22:19:33 -0300

    update producction url

--- donutshomero/utils/admin.js
+++ donutshomero/utils/admin.js
@@ -13,7 +13,7 @@ export const getSingleDonuts = async (setState) => {
 export const getOrders = async (setState, skip, take) => {
   const response = await axios.get(
     process.env.NODE_ENV === "production"
-      ? "https://donutshomero.vercel.app/api/admin/orders?skip=${skip}&take=${take}"
+      ? `https://donutshomero.vercel.app/api/admin/orders?skip=${skip}&take=${take}`
       : `http://localhost:3000/api/admin/orders?skip=${skip}&take=${take}`
   );
   setState(response.data);
